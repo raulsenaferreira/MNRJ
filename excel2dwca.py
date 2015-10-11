3c3cc8fd063d412c66216b769f30c2591acef66c
Raul Sena Ferreira <raulsf@cos.ufrj.br>
2015-10-11 23:00:04 +0000

    Update excel2dwca.py

--- excel2dwca.py
+++ excel2dwca.py
@@ -1,6 +1,15 @@
-#Created by Raul S. Ferreira raulsf@cos.ufrj.br
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 11 18:13:17 2015
+
+@author: Raul
+"""
+
+from __future__ import absolute_import, division, print_function
 import subprocess
-from pprint import pprint as pp
+from util import Util
+#from pprint import pprint as pp
+import csv     # imports the csv module
 
 # deve ser trocado por endereco do arquivo a ser modificado e arquivo destino
 file = "TESTE.xlsx result.csv"
@@ -8,4 +17,17 @@ file = "TESTE.xlsx result.csv"
 #chama o shell e converte planilha para csv
 argument = 'ssconvert {0}'.format(file)
 command_process = subprocess.call([argument], shell=True)
-#pp(command_process)
+
+ofile  = open('final.csv', "wb")
+writer = csv.writer(ofile, delimiter=',')
+
+reader=[]
+f = open("result.csv", 'rb') # opens the csv file
+try:
+    reader = csv.reader(f)  # creates the reader object
+    for row in reader:   # iterates the rows of the file in orders
+        row[0] = row[0]+"Raul"        
+        writer.writerow(row)    # prints each row
+finally:
+    f.close()
+    
