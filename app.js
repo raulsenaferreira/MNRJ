7574535bfee3d6efc7302cc5f23a722468a47bea
Raul <raulsenaferreira>
2015-10-13 12:17:20 -0300

    primeiro teste com sucesso usando as diretivas do Angular

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-var app = angular.module('main', []);
+var app = angular.module('modulo', []);
+	app.controller('controle', ['$scope', function($scope){
+		
+		$scope.sobreNome = function(){
+			$scope.texto = 'sena ferreira';
+		};
+	}]);
