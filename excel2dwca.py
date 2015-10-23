104d976fb7a770f494da7d98badaacbe0d528a01
Raul <raulsenaferreira@gmail.com>
2015-10-23 13:37:44 -0200

    inclusão de integração entre python e node.js

--- excel2dwca.py
+++ excel2dwca.py
@@ -6,24 +6,27 @@ Created on Sun Oct 11 18:13:17 2015
 """
 
 from __future__ import absolute_import, division, print_function
+from sys import stdin
 import subprocess
 #from util import Util
 #from pprint import pprint as pp
 import csv     # imports the csv module
 
-def main():
+def main(file):
+    #parametro file contem nome do arquivo original e sua extensao
     # deve ser trocado por endereco do arquivo a ser modificado e arquivo destino
-    file = "TESTE.xlsx result.csv"
-    
+    #file = "TESTE.xlsx result.csv"
+    files = file.split('.')
     #chama o shell e converte planilha para csv
-    argument = 'ssconvert {0}'.format(file)
+    argument = 'ssconvert {0}'.format(file+' '+files[0]+'.csv')
     command_process = subprocess.call([argument], shell=True)
     
-    ofile  = open('final.csv', "wb")
+    ofile  = open(files[0]+'-CONVERTIDO.csv', "wb")#open('final.csv', "wb")
     writer = csv.writer(ofile, delimiter=',')
     
     header=True
-    f = open("result.csv", 'rb') # opens the csv file
+    f = open(files[0]+'.csv', 'rb')#open("result.csv", 'rb') # opens the csv file
+    
     try:
         reader = csv.reader(f)  # creates the reader object
         for row in reader:   # iterates the rows of the file in orders
@@ -34,25 +37,97 @@ def main():
             writer.writerow(row)    # prints each row
     finally:
         f.close()
+        return True
     
-    
+#DATA ENTRADA aaaa-mm-dd	 = eventDate ?
+#COLECAO DE ORIGEM	= institutionCode ?
     
 def mapDWC(term):
         term = str(term).lower().replace(' ','')
         mDWC = dict()
+        
+        #mapeamentos por fazer
+        mDWC[''] = 'minimumElevationInMeters'
+        mDWC[''] = 'maximumElevationInMeters'
+        mDWC[''] = 'type'
+        mDWC[''] = 'modified'
+        mDWC[''] = 'language'
+        mDWC[''] = 'rights'
+        mDWC[''] = 'rightsHolder'
+        mDWC[''] = 'institutionID'
+        mDWC[''] = 'institutionCode'
+        mDWC[''] = 'collectionCode'
+        mDWC[''] = 'datasetName'
+        mDWC[''] = 'occurrenceID'
+        mDWC[''] = 'individualCount'
+        mDWC[''] = 'preparations'
+        mDWC[''] = 'habitat'
+        mDWC[''] = 'continent'
+        mDWC[''] = 'countryCode'
+        mDWC[''] = 'county'
+        mDWC[''] = 'minimumDepthInMeters'
+        mDWC[''] = 'maximumDepthInMeters'
+        mDWC[''] = 'locationRemarks'
+        mDWC[''] = 'georeferencedBy'
+        mDWC[''] = 'georeferencedDate'
+        mDWC[''] = 'georeferenceProtocol'
+        mDWC[''] = 'typeStatus'
+        mDWC[''] = 'scientificName'
+        mDWC[''] = 'Dinamic properties'
+        mDWC[''] = 'taxonRank'
+        
+        mDWC['subfamilia'] = ''
+        mDWC['tribo'] = ''
+        mDWC['ambiente'] = ''
+        mDWC['entrada'] = ''
+        mDWC['dataentrada'] = ''
+        mDWC['numerocoletor'] = ''
+        mDWC['minutodelatitude'] = ''
+        mDWC['segundodelatitude'] = ''
+        mDWC['latitudenortesul'] = ''
+        mDWC['minutodelongitude'] = ''
+        mDWC['segundodelongitude'] = ''
+        mDWC['longitudelesteoeste'] = ''
+        mDWC['nºindividuos'] = ''
+        mDWC['projeto'] = ''
+        mDWC['disponibilidade'] = ''
+        mDWC['numeroreservado'] = ''
+        mDWC['subfilo'] = ''
+        mDWC['superordem'] = ''
+        mDWC['superfamilia'] = ''
+        mDWC['subfamilia'] = ''
+        mDWC['sexo'] = ''
+        mDWC['tipodecolecao'] = ''
+        mDWC['hospedeiro'] = ''
+        mDWC['dna'] = ''
+        mDWC['pecaanatomica'] = ''
+        mDWC['nous'] = ''
+        mDWC['eouw'] = ''
+        mDWC['regiao'] = ''
+        mDWC['habito'] = ''
+        mDWC['litologia'] = ''
+        mDWC['ecossistema'] = ''
+        mDWC['fenologia'] = ''
+        mDWC['idadegeologica'] = ''
+        mDWC['baciasedimentar'] = ''
+        mDWC['numerodocoletor'] = ''
+        mDWC['familiaantiga'] = ''
+        mDWC['generoantigo'] = ''
+        mDWC['epitetoantigo'] = ''
+        mDWC['determinadorantigo'] = ''
+        
+        #mapeamentos feitos
         mDWC['filo'] = 'phylum'
         mDWC['classe'] = 'class'
         mDWC['ordem'] = 'order'
         mDWC['familia'] = 'family'
-        #mDWC['subfamilia'] = ''
-        #mDWC['tribo'] = ''
+        mDWC['registro'] = 'catalogNumber'
+        mDWC['observacaodoregistro'] = 'occurrenceRemarks'
         mDWC['genero'] = 'genus'
         mDWC['epiteto'] = 'specificEpithet'
         mDWC['numerodecampo'] = 'fieldNumber'
-        #mDWC['ambiente'] = ''
+        mDWC['reino'] = 'kingdom'
         mDWC['datadedeterminacao'] = 'dateIdentified'
-        #mDWC['entrada'] = ''
-        #mDWC['dataentrada'] = ''
         mDWC['tecnicacol'] = 'samplingProtocol'
         mDWC['tipodecolecao'] = 'type'
         mDWC['observacaoreg'] = 'basisOfRecord'
@@ -60,26 +135,17 @@ def mapDWC(term):
         mDWC['estado'] = 'stateProvince'
         mDWC['municipio'] = 'municipality'
         mDWC['localidade'] = 'locality'
-        #mDWC['numerocoletor'] = ''
         mDWC['datacoleta'] = 'eventDate'
         mDWC['observacaocoleta'] = 'eventRemarks'
         mDWC['graudelatitude'] = 'decimalLatitude'
-        #mDWC['minutodelatitude'] = ''
-        #mDWC['segundodelatitude'] = ''
-        #mDWC['latitudenortesul'] = ''
         mDWC['graudelongitude'] = 'decimalLongitude'
-        #mDWC['minutodelongitude'] = ''
-        #mDWC['segundodelongitude'] = ''
-        #mDWC['longitudelesteoeste'] = ''
         mDWC['profundidade'] = 'verbatimDepth'
-        #mDWC['nºindividuos'] = ''
         mDWC['determinador'] = 'identifiedBy'
         mDWC['coletor'] = 'recordedBy'
-        #mDWC['projeto'] = ''
         
         try:
             return mDWC[term]
         except KeyError:
             return term
             
-main()
+main(stdin.readline())
---
+++ server.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var multer = require('multer');
+var app = express();
+var http = require('http').Server(app);
+var upload = multer({ dest: './uploads/'});
+var PythonShell = require('python-shell');
+
+app.use("/css", express.static(__dirname + '/css'));
+app.use("/js", express.static(__dirname + '/js'));
+app.use("/img", express.static(__dirname + '/img'));
+app.use("/fonts", express.static(__dirname + '/fonts'));
+app.use("/font-awesome/css/", express.static(__dirname + '/font-awesome/css/'));
+
+app.use(multer({ dest: './uploads/',
+    rename: function (fieldname, filename) {
+        return filename+Date.now();
+    },
+    onFileUploadStart: function (file) {
+        console.log('Subindo arquivo' + file.originalname + ' ...');
+    },
+    onFileUploadComplete: function (file) {
+        console.log(file.fieldname + ' salvo em  ' + file.path)
+        filePath = file.path.split('.')[0];
+        executePython(filePath);
+    }
+}));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname +'/index.html');
+});
+
+app.post('/api/photo',function(req,res){
+    upload(req,res,function(err) {
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+        res.end("Upload de arquivo terminado com sucesso!");
+    });
+});
+
+function executePython(filePath){
+  var options = {
+    //mode: 'text',
+    //pythonPath: 'path/to/python',
+    //pythonOptions: ['-u'],
+    //scriptPath: 'path/to/my/scripts',
+    //args: ['value1', 'value2', 'value3']
+    args: [filePath]
+  };
+
+  PythonShell.run('excel2dwca.py', options, function (err, results) {//excel2dwca
+    if (err) throw err;
+    // results is an array consisting of messages collected during execution
+    console.log('results: %j', results);
+    finalFile = options.args
+    console.log('Arquivo disponivel em ' + __dirname + '/' + finalFile + '-CONVERTIDO.csv')
+  });
+}
+
+http.listen(4000, function(){
+  console.log('Disponível na porta 4000');
+});
