b1571c149c4d0f8540c8b253a619cfd6dd5ef710
Raul <raulsenaferreira@gmail.com>
2015-10-28 13:06:04 -0200

    delete node modules folder

--- app.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('modulo', []);
-	app.controller('controle', ['$scope', function($scope){
-
-		$scope.sobreNome = function(){
-			$scope.texto = 'sena ferreira';
-		};
-	}]);
