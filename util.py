d5cea05abdc8e1093121ac9ad4fd582bd1011213
Raul <raulsenaferreira@gmail.com>
2015-11-12 14:24:55 -0200

    updating

--- util.py
+++ util.py
@@ -4,50 +4,22 @@ Created on Sun Oct 11 22:17:15 2015
 
 @author: raul
 """
+def main():
+    degLat = ('45', '32', '25', 'N')
+    degLng = ('129', '40', '31', 'W')
+    lat, lng = converte(degLat, degLng)
+    print(lat)
+    print(lng)
 
+def converte(degLat, degLng):
+    lat = int(degLat[0]) + float(degLat[1]) / 60 + float(degLat[2]) / 3600
+    if degLat[3]=='S':
+        lat*=-1
 
-class Util(object):
-    
-    def mapDWC(term):
-        mDWC = dict()
-        mDWC['filo'] = 'phylum'
-        mDWC['classe'] = 'class'
-        mDWC['ordem'] = 'order'
-        mDWC['familia'] = 'family'
-        mDWC['subfamilia'] = ''
-        mDWC['tribo'] = ''
-        mDWC['genero'] = 'genus'
-        mDWC['epiteto'] = 'specificEpithet'
-        mDWC['numerodecampo'] = 'fieldNumber'
-        mDWC['ambiente'] = ''
-        mDWC['datadedeterminacao'] = 'dateIdentified'
-        mDWC['entrada'] = ''
-        mDWC['dataentrada'] = ''
-        mDWC['tecnicacol'] = 'samplingProtocol'
-        mDWC['tipodecolecao'] = 'type'
-        mDWC['observacaoreg'] = 'basisOfRecord'
-        mDWC['pais'] = 'country'
-        mDWC['estado'] = 'stateProvince'
-        mDWC['municipio'] = 'municipality'
-        mDWC['localidade'] = 'locality'
-        mDWC['numerocoletor'] = ''
-        mDWC['datacoleta'] = 'eventDate'
-        mDWC['observacaocoleta'] = 'eventRemarks'
-        mDWC['graudelatitude'] = 'decimalLatitude'
-        mDWC['minutodelatitude'] = ''
-        mDWC['segundodelatitude'] = ''
-        mDWC['latitudenortesul'] = ''
-        mDWC['graudelongitude'] = 'decimalLongitude'
-        mDWC['minutodelongitude'] = ''
-        mDWC['segundodelongitude'] = ''
-        mDWC['longitudelesteoeste'] = ''
-        mDWC['profundidade'] = 'verbatimDepth'
-        mDWC['nºindividuos'] = ''
-        mDWC['determinador'] = 'identifiedBy'
-        mDWC['coletor'] = 'recordedBy'
-        mDWC['projeto'] = ''
-        
-        try:
-            return mDWC[term]
-        except KeyError:
-            return term
+    lng = int(degLng[0]) + float(degLng[1]) / 60 + float(degLng[2]) / 3600
+    if degLng[3]=='W':
+        lng*=-1
+
+    return (lat, lng)
+
+main()
